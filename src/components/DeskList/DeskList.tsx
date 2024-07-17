e62b46d3b596ad89dd9c39d60c2bf927bcf61253
Artem <riabykhartem@yandex.ru>
2024-07-16 22:13:31 -0500

    refactor/splited components

--- src/components/DeskList/DeskList.tsx
+++ src/components/DeskList/DeskList.tsx
@@ -1,14 +1,13 @@
-import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
-import { getDesks, getLocations } from '../../api/api';
+import {  useEffect, useState } from 'react';
+import { getDesks } from '../../api/api';
 import { Desk } from '../../api/desk.model';
-import { Location } from '../../api/location.model.ts';
 import { MainLayout } from '../../layouts/MainLayout.tsx';
 import { DeskCard } from '../DeskCard/DeskCard.tsx';
 import { List, ListItem } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
 
 export interface DeskListProps {
   locationId: string;
+  searchValue: string;
 }
 
 export const DeskList = (props: DeskListProps) => {
@@ -16,50 +15,18 @@ export const DeskList = (props: DeskListProps) => {
   console.log('DeskList is rendering...');
 
   const [desks, setDesks] = useState<Desk[]>([]);
-  const [locations, setLocations] = useState<Location[]>([]);
 
-  const searchInputRef = useRef<HTMLInputElement>(null);
-
-  const navigate = useNavigate();
 
   useEffect(() => {
-    getDesks({locationId: props.locationId})
+    getDesks({locationId: props.locationId, searchTerm: props.searchValue})
       .then(results => {
         setDesks(results);
       });
 
-    getLocations()
-      .then(locations => {
-        setLocations(locations);
-      });
-  }, []);
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const q = searchInputRef.current?.value ?? '';
-
-    getDesks({locationId: props.locationId, searchTerm: q})
-      .then(results => {
-        setDesks(results);
-      });
-  }
-
-  const handleLocationChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    navigate(`/${e.target.value}/desks`);
-  }
+  }, [props.locationId, props.searchValue]);
 
   return (
     <MainLayout>
-      <select value={props.locationId} onChange={handleLocationChange}>
-        {locations.map(location => (
-          <option key={location.id} value={location.id}>{location.displayName}</option>
-        ))}
-      </select>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Search for desk" ref={searchInputRef} />
-        <button type="submit">Search</button>
-      </form>
       <List>
         {desks.map((desk) => (
           <ListItem key={desk.id}>
--- src/pages/DesksPage.tsx
+++ src/pages/DesksPage.tsx
@@ -1,12 +1,27 @@
 import { DeskList } from '../components/DeskList/DeskList.tsx';
 import { useParams } from 'react-router-dom';
+import {DeskSearch} from '../components/DeskSearch/DeskSearch.tsx'
+import { ChangeEvent, useState, useEffect } from 'react';
+// import { getDesks } from '../api/api.ts';
+// import { Desk } from '../api/desk.model';
+
 
 export const DesksPage = () => {
+  console.log("is rendering");
+  const [searchValue, setSearchValue] = useState('')
+
   const {locationId} = useParams();
   if (!locationId)
     throw new Error('locationId param is missing');
 
+  const handleSearch = (value: string) =>{
+    setSearchValue(value)
+
+  }
   return (
-    <DeskList key={locationId} locationId={locationId} />
-  );
+    <>
+    <DeskSearch locationId={locationId} handleSearch={handleSearch}/>
+    <DeskList key={locationId} locationId={locationId} searchValue={searchValue}/>
+    </>
+  )
 }
