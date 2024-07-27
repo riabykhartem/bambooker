617f1a576ae41e18952b34f75902238a6953e626
Ilia Popov <me.ilya.popov@gmail.com>
2024-07-27 15:07:51 -0500

    Moved desk state to DeskPage component

--- src/components/DeskList/DeskList.tsx
+++ src/components/DeskList/DeskList.tsx
@@ -1,37 +1,22 @@
-import { useEffect, useState } from 'react';
-import { getDesks } from '../../api/api';
 import { Desk } from '../../api/desk.model';
 import { MainLayout } from '../../layouts/MainLayout.tsx';
 import { DeskCard } from '../DeskCard/DeskCard.tsx';
 import { List, ListItem } from '@mui/material';
 
-
 export interface DeskListProps {
   locationId: string;
-  searchValue: string;
+  desks: Desk[];
 }
 
 export const DeskList = (props: DeskListProps) => {
-
   console.log('DeskList is rendering...');
-
-  const [desks, setDesks] = useState<Desk[]>([]);
-
-
-  useEffect(() => {
-    getDesks({ locationId: props.locationId, searchTerm: props.searchValue })
-      .then(results => {
-        setDesks(results);
-      });
-
-  }, [props.locationId, props.searchValue]);
 
   return (
     <MainLayout>
       <List sx={{
         width: '360px'
       }}>
-        {desks.map((desk) => (
+        {props.desks.map((desk) => (
           <ListItem key={desk.id}>
             <DeskCard {...desk} />
           </ListItem>
--- src/pages/DesksPage.tsx
+++ src/pages/DesksPage.tsx
@@ -3,26 +3,32 @@ import { useParams } from 'react-router-dom';
 import { DeskListToolbar } from '../components/DeskListToolbar/DeskListToolbar.tsx'
 import { useState } from 'react';
 import { AppBar, Toolbar } from "@mui/material";
+import { Desk } from "../api/desk.model.ts";
+import { getDesks } from "../api/api.ts";
 
 export const DesksPage = () => {
-  console.log("is rendering");
-  const [searchValue, setSearchValue] = useState('')
+  console.log('DesksPage is rendering');
 
+  const [desks, setDesks] = useState<Desk[]>([]);
   const { locationId } = useParams();
+
   if (!locationId)
     throw new Error('locationId param is missing');
 
-  const handleSearch = (value: string) => {
-    setSearchValue(value)
+  const handleSearch = (searchTerm: string) => {
+    getDesks({ locationId: locationId, searchTerm: searchTerm })
+      .then(results => {
+        setDesks(results);
+      });
+  };
 
-  }
   return (
     <>
       <AppBar position="fixed" color="inherit">
         <DeskListToolbar locationId={locationId} handleSearch={handleSearch}/>
       </AppBar>
       <Toolbar/>
-      <DeskList key={locationId} locationId={locationId} searchValue={searchValue}/>
+      <DeskList key={locationId} locationId={locationId} desks={desks}/>
     </>
   )
 }
